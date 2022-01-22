819c039dbae718235fa24d61ee0de34446843faf
fisher60 <wakefollower@gmail.com>
2022-01-22 16:25:16 -0600

    update to new api url

--- bot/settings.py
+++ bot/settings.py
@@ -9,6 +9,6 @@ DEBUG = True if environ.get("DEBUG") in ["true", "True", "TRUE"] else False
 
 BOT_TOKEN = environ.get("BOT_TOKEN")
 
-API_BASE_URL = "https://www.sleepykittyguild.com"
+API_BASE_URL = "https://api.sleepykittyguild.com"
 
 TEST_GUILD_IDS = [438583304783134731]
